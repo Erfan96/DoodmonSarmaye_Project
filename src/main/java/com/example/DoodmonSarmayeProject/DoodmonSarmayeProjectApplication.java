cadbaa8ee5001be1fdc9ff0265302013f38c5df8
efsam <E.salimi75@ymail.com>
2021-02-22 16:20:52 +0330

    commit DoodmonSarmayeProjectApplication.java in master

---
+++ src/main/java/com/example/DoodmonSarmayeProject/DoodmonSarmayeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.DoodmonSarmayeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoodmonSarmayeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoodmonSarmayeProjectApplication.class, args);
+	}
+
+}
