55cc73810fe4c1a4c0c4309cbc5ef172af3a78f1
efsam <E.salimi75@ymail.com>
2021-02-25 12:05:46 +0330

    commit CustomAuthenticationSuccessHandler.java in secondary

---
+++ src/main/java/com/example/DoodmonSarmayeProject/configure/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,38 @@
+package com.example.DoodmonSarmayeProject.configure;
+
+import com.example.DoodmonSarmayeProject.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+
+        System.out.println("\n\nIn customAuthenticationSuccessHandler\n\n");
+
+        String username = authentication.getName();
+
+        System.out.println("username" + username);
+
+        UserDetails theUser = userService.loadUserByUsername(username);
+
+        HttpSession session = httpServletRequest.getSession();
+        session.setAttribute("user", theUser);
+
+        httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/");
+    }
+}
