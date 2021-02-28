31d61aa208a3c9c9486a00b7d2531460df615bfb
efsam <E.salimi75@ymail.com>
2021-02-28 17:08:28 +0330

    commit RegisterService.java in secondary

---
+++ src/main/java/com/example/DoodmonSarmayeProject/service/RegisterService.java
@@ -0,0 +1,50 @@
+package com.example.DoodmonSarmayeProject.service;
+
+import com.example.DoodmonSarmayeProject.entities.Role;
+import com.example.DoodmonSarmayeProject.entities.User;
+import com.example.DoodmonSarmayeProject.repository.UserRepository;
+import com.example.DoodmonSarmayeProject.user.FrontUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class RegisterService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = this.userRepository.findByUsername(username);
+        return user.orElse(null);
+    }
+
+    @Transactional
+    public void saveUser(FrontUser fUser) {
+        User user = new User();
+
+        user.setUsername(fUser.getUserName());
+        user.setPassword(passwordEncoder.encode(fUser.getPassword()));
+        user.setFirstName(fUser.getFirstName());
+        user.setLastName(fUser.getLastName());
+        user.setNationalCode(fUser.getNationalCode());
+        user.setPhoneNumber(fUser.getPhoneNumber());
+        user.setEmail(fUser.getEmail());
+
+        user.grantedAuthority(Role.ROLE_INVESTOR);
+
+        this.userRepository.save(user);
+    }
+}
