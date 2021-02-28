a3b5b24f84099a5988cdc3f69fe389d866c48d7b
efsam <E.salimi75@ymail.com>
2021-02-28 17:07:19 +0330

    commit UserService.java in secondary

--- src/main/java/com/example/DoodmonSarmayeProject/service/UserService.java
+++ src/main/java/com/example/DoodmonSarmayeProject/service/UserService.java
@@ -1,12 +1,16 @@
 package com.example.DoodmonSarmayeProject.service;
 
+import com.example.DoodmonSarmayeProject.entities.Role;
 import com.example.DoodmonSarmayeProject.entities.User;
 import com.example.DoodmonSarmayeProject.repository.UserRepository;
+import com.example.DoodmonSarmayeProject.user.FrontUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -16,14 +20,10 @@ public class UserService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
-//    @Autowired
-//    public UserService(UserRepository userRepository) {
-//        this.userRepository = userRepository;
-//    }
-
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<User> user = userRepository.findByUsername(username);
+        Optional<User> user = this.userRepository.findByUsername(username);
         if (user.isPresent()) {
            return user.get();
         }
