dc0e0a4516a59811b23556fcee90f9d31e7da495
efsam <E.salimi75@ymail.com>
2021-03-03 19:51:38 +0330

    commit AdminController.java in secondary

--- src/main/java/com/example/DoodmonSarmayeProject/controller/AdminController.java
+++ src/main/java/com/example/DoodmonSarmayeProject/controller/AdminController.java
@@ -1,29 +1,26 @@
 package com.example.DoodmonSarmayeProject.controller;
 
-import com.example.DoodmonSarmayeProject.service.RegisterService;
+import com.example.DoodmonSarmayeProject.entities.User;
+import com.example.DoodmonSarmayeProject.service.UserService;
 import com.example.DoodmonSarmayeProject.user.FrontUser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
     @Autowired
-    private RegisterService registerService;
+    private UserService userService;
 
     @GetMapping("/adminPage")
     public String adminPage() {
-        return "/admin/adminPage.html";
+        return "/admin/adminPage";
     }
 
     @GetMapping("/signup")
@@ -31,20 +28,12 @@ public class AdminController {
         return "signup-page";
     }
 
-//    @PostMapping("/add")
-//    public String addNewUser(@Valid @ModelAttribute("frontUser") FrontUser frontUser
-//                            , BindingResult result, Model model) {
-//
-//        if (result.hasErrors()) {
-//            return "signup-page";
-//        }
-//
-//        UserDetails existing = registerService.loadUserByUsername(frontUser.getUserName());
-//        if (existing != null) {
-//            return "signup-page";
-//        }
-//
-//        registerService.saveUser(frontUser);
-//        return "/admin/adminPage.html";
-//    }
+    @GetMapping("/list")
+    public String getAllUsers(Model model) {
+
+        List<User> userList = userService.findAll();
+        model.addAttribute("users", userList);
+
+        return "/admin/list-users";
+    }
 }
