9b60c040b7c06a19b6b6673ef3ffa4435bd6a762
efsam <E.salimi75@ymail.com>
2021-03-14 10:41:31 +0330

    commit Request.java in secondary

---
+++ src/main/java/com/example/DoodmonSarmayeProject/entities/Request.java
@@ -0,0 +1,40 @@
+package com.example.DoodmonSarmayeProject.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "request_investor")
+public class Request {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private LocalDateTime date;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Subject subject;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @ManyToOne
+    @JoinColumn
+    private User user;
+
+    @NotBlank(message = "شرح درخواست نمی تواند خالی باشد")
+    @Size(max = 4000, message = "طول پیام بیشتر از حد تعیین شده است")
+    private String description;
+
+    
+}
