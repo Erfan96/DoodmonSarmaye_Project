d4f595ccdedb729772d564e6ebb48acd88496a17
efsam <E.salimi75@ymail.com>
2021-03-15 11:23:07 +0330

    commit ResponsibleController.java in secondary

--- src/main/java/com/example/DoodmonSarmayeProject/controller/ResponsibleController.java
+++ src/main/java/com/example/DoodmonSarmayeProject/controller/ResponsibleController.java
@@ -1,18 +1,18 @@
 package com.example.DoodmonSarmayeProject.controller;
 
 import com.example.DoodmonSarmayeProject.entities.Request;
-import com.example.DoodmonSarmayeProject.entities.Subject;
+import com.example.DoodmonSarmayeProject.entities.Response;
 import com.example.DoodmonSarmayeProject.entities.User;
 import com.example.DoodmonSarmayeProject.service.RequestService;
-import com.example.DoodmonSarmayeProject.service.SubjectService;
+import com.example.DoodmonSarmayeProject.service.ResponseService;
 import com.example.DoodmonSarmayeProject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -26,22 +26,13 @@ public class ResponsibleController {
     private RequestService requestService;
 
     @Autowired
-    private SubjectService subjectService;
+    private ResponseService responseService;
 
     @GetMapping("/listRequests")
     public String showListOfRequest(Model model) {
         List<Request> requests = requestService.getAllRequest();
         model.addAttribute("requests", requests);
         return "/responsible/listRequest-page";
-    }
-
-    @GetMapping("/findRequest")
-    public String detailOfRequest(@RequestParam("Id") Long id, Model model) {
-        Request request = this.requestService.getRequestById(id);
-        List<Subject> subjects = this.subjectService.findAllSubjects();
-        model.addAttribute("subjects", subjects);
-        model.addAttribute("request", request);
-        return "/responsible/detailRequest-page";
     }
 
     @GetMapping("/information")
@@ -56,5 +47,27 @@ public class ResponsibleController {
         List<Request> requests = this.requestService.getAllRequestByUser(userService.loadUserByID(id));
         model.addAttribute("requests", requests);
         return "/responsible/listRequestForAnInvestor-page";
+    }
+
+    @GetMapping("/findRequest")
+    public String detailOfRequest(@RequestParam("Id") Long id, Model model) {
+        Request request = this.requestService.getRequestById(id);
+        Response response = this.responseService.getResponseByRequest(request);
+        model.addAttribute("request", request);
+        model.addAttribute("response", response);
+        return "/responsible/detailRequest-page";
+    }
+
+    @PostMapping("/sendResponse")
+    public String sendResponse(@Valid @ModelAttribute("response")Response response,
+                               BindingResult result, Model model) {
+
+        if (result.hasErrors()) {
+            return "/responsible/detailRequest-page";
+        }
+
+        this.responseService.saveResponse(response);
+        this.requestService.changeStatus(response.getRequest().getId());
+        return "redirect:/responsible/listRequests";
     }
 }
